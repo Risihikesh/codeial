6a8cd4b76116520ebefd06f37f4ac2d169ae5960
Rishikesh <krishikesh369@gmail.com>
2023-05-21 00:44:14 +0530

    implementing partial

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,8 @@
 module.exports.profile= function(req, res){
     res.end('<h1>User Profile</h1>');
+
+    //niche wala nhi ho rha 
+    // return res.render('profile',{
+    //     title: "User Profile"
+    // });
 }
