524900d44d184b69d8cc3e04c276a2c7c5c26590
Rishikesh <krishikesh369@gmail.com>
2023-05-20 11:00:18 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express= require('express');
 const app=express();
 const port=8000;
 
-
+//use express router
+app.use('/',require('./routes/index')) // or we can just use /routes it will automatically fetch index.js in routes
 
 app.listen(port, function(err){
     if(err){
